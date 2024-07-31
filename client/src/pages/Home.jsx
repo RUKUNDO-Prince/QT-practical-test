a6e473385d101f66137e3122ba540ddc9387d6d4
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-07-31 12:32:29 +0300

    finished the homepage, added some dummy data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { posts } from '../constants/data';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="posts">
+        {posts.map(post => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="img" />
+            </div>
+            <div className="content">
+              <Link className='link' to={`/post/${post.id}`}><h1>{post.title}</h1></Link>
+              <p>{post.desc}</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
