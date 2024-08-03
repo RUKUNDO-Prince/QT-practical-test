489b47cee4ece9280e8c64bb801c2287e372592c
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-08-03 12:45:46 +0300

    limited the blogs and content to be displayed in the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-// import { posts } from '../constants/data';
-import axios from 'axios'
+import axios from 'axios';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [visiblePosts, setVisiblePosts] = useState(6);
 
   const category = useLocation().search;
 
@@ -16,33 +16,46 @@ const Home = () => {
       } catch (err) {
         console.log(err);
       }
-    }
+    };
     fetchData();
   }, [category]);
 
   const getText = (html) => {
-    const doc = new DOMParser().parseFromString(html, "text/html");
+    const doc = new DOMParser().parseFromString(html, 'text/html');
     return doc.body.textContent;
-  }
+  };
+
+  const handleShowMore = () => {
+    setVisiblePosts((prev) => prev + 3);
+  };
 
   return (
-    <div className='home'>
+    <div className="home">
       <div className="posts">
-        {posts.map(post => (
+        {posts.slice(0, visiblePosts).map((post) => (
           <div className="post" key={post.id}>
             <div className="img">
               <img src={`./uploads/${post.img}`} alt="img" />
             </div>
             <div className="content">
-              <Link className='link' to={`/post/${post.id}`}><h1>{post.title}</h1></Link>
-              <p>{getText(post.content)}</p>
-              <button>Read More</button>
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p className="blog-content">{getText(post.content)}</p>
+              <Link to={`/post/${post.id}`}>
+                <button>Read More</button>
+              </Link>
             </div>
           </div>
         ))}
       </div>
+      {visiblePosts < posts.length && (
+        <button onClick={handleShowMore} className="show-more">
+          Show More
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
